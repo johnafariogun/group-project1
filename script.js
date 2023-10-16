28a47f206e0f2ee10ab463a2bcf4d3eef18f22a5
John Tolulope Afariogun <afariogun.john@yahoo.com>
2023-10-16 20:22:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,3 @@
-// function detectNumber() {
-//     const phoneNumber = document.getElementById('phoneNumber').value;
-//     const result = document.getElementById('result');
-    
-
-//     if (phoneNumber !== '') {
-//         let number = phoneNumber.split(" ").join('')
-        
-//         if (number.length >= 11) {
-//             if (number.substring(0, 4) === "+234"){
-//                 number = 0 + number.slice(4);
-//             }
-//             console.log(number);
-//             const prefix = number.substring(0, 4);
-//             if (prefix === '0803' ||
-//             prefix === '0703' ||
-//             prefix === '0903' ||
-//             prefix === '0806' ||
-//             prefix === '0706' ||
-//             prefix === '0813' ||
-//             prefix === '0810' ||
-//             prefix === '0814' ||
-//             prefix === '0816') {
-//             result.textContent = 'Mobile Provider: MTN';
-//         } else if (prefix === '0805' ||
-//             prefix === '0705' ||
-//             prefix === '0905' ||
-//             prefix === '0807' ||
-//             prefix === '0815' ||
-//             prefix === '0811' ||
-//             prefix === '0905') {
-//             result.textContent = 'Mobile Provider: GLO';
-//         } else if (prefix === '0809' ||
-//             prefix === '0909' ||
-//             prefix === '0817' ||
-//             prefix === '0818') {
-//             result.textContent = 'Mobile Provider: 9MOBILE';
-//         } else if (prefix === '0802' ||
-//             prefix === '0902' ||
-//             prefix === '0701' ||
-//             prefix === '0808' ||
-//             prefix === '0708' ||
-//             prefix === '0812') {
-//             result.textContent = 'Mobile Provider: AIRTEL';
-//         } else {
-//             result.textContent = 'Mobile Provider: UNKNOWN';
-//         }
-           
-//         } else {
-//             result.textContent = 'Please enter a valid phone number.';
-//         }
-//     } else {
-//         result.textContent = 'Phone number field cannot be empty.';
-//     }
-// }
-
 function detectNumber() {
     const phoneNumber = document.getElementById('phoneNumber').value;
     const result = document.getElementById('result');
@@ -71,13 +15,13 @@ function detectNumber() {
             // Create an image element
             let img = new Image();
             img.src = imageSource;
-            img.width = 100; // Set the width of the image
-            img.height = 100; // Set the height of the image
+            img.width = 100; 
+            img.height = 100; 
 
-            // Clear the previous result text
+           
             result.textContent = '';
 
-            // Append the image to the result div
+            
             result.appendChild(img);
         } else {
             result.textContent = 'Please enter a valid phone number.';
@@ -98,7 +42,7 @@ function getOperatorImageSource(prefix) {
         case '0810':
         case '0814':
         case '0816':
-            return 'mtn.png'; // Replace with the actual MTN image source
+            return 'mtn.png'; 
 
         case '0805':
         case '0705':
@@ -107,13 +51,13 @@ function getOperatorImageSource(prefix) {
         case '0815':
         case '0811':
         case '0905':
-            return 'Glo_Limited.png'; // Replace with the actual GLO image source
+            return 'Glo_Limited.png'; 
 
         case '0809':
         case '0909':
         case '0817':
         case '0818':
-            return '9mobile.png'; // Replace with the actual 9MOBILE image source
+            return '9mobile.png'; 
 
         case '0802':
         case '0902':
@@ -121,10 +65,10 @@ function getOperatorImageSource(prefix) {
         case '0808':
         case '0708':
         case '0812':
-            return 'airtel.png'; // Replace with the actual AIRTEL image source
+            return 'airtel.png'; 
 
         default:
-            return ''; // Replace with the actual UNKNOWN image source
+            return ''; 
     }
 }
 
